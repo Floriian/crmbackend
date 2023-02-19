253b870000a919d74e02c2da5357e46163c7f173
Floriian <lovasifloriian@outlook.hu>
2023-02-19 22:32:49 +0100

    basic controllers.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { MongooseModule } from '@nestjs/mongoose';
+import { TasksModule } from './tasks/tasks.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import configYaml from './config/configuration';
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configYaml],
+    }),
+    MongooseModule.forRoot(
+      'mongodb://127.0.0.1:27017/crm?readPreference=primary&ssl=false&directConnection=true',
+    ),
+    TasksModule,
+    AuthModule,
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,12 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  getHello() {
+    const res = {
+      name: 'szia',
+      text: 'asd',
+      abc: 123,
+    };
+    return res;
   }
 }
