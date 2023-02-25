ac6ea91f125d5861e1ec5d84de9c6150af4158ee
Floriian <lovasifloriian@outlook.hu>
2023-02-25 10:16:16 +0100

    remove console.log

--- src/config/configuration.ts
+++ src/config/configuration.ts
@@ -5,7 +5,6 @@ import { join } from 'path';
 const YAML_FILENAME = './config.yaml';
 
 export default () => {
-  console.log(readFileSync(join(__dirname, YAML_FILENAME), 'utf8'));
   return yaml.load(
     readFileSync(join(__dirname, YAML_FILENAME), 'utf8'),
   ) as Record<string, any>;
