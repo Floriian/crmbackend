2c62bded65ad0380ae9b1661a3ebe81f4f2dc66c
Floriian <lovasifloriian@outlook.hu>
2023-02-26 18:23:02 +0100

    remove default controller & service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
@@ -15,13 +13,11 @@ import configYaml from './config/configuration';
       load: [configYaml],
     }),
     MongooseModule.forRoot(
-      'mongodb://crmuser:crmpassword@127.0.0.1:27017/crm?readPreference=primary&ssl=false&directConnection=true',
+      'mongodb://crmuser:crmpassword@localhost:27017/crm?readPreference=primary&ssl=false&directConnection=true',
     ),
     AuthModule,
     UserModule,
     TasksModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
